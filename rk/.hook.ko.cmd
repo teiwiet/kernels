8cc61a49dfdb83c9f07d8bcdecd8df8eeca09349
teiwiet <teiwiet33@gmail.com>
2026-04-04 04:21:48 +0700

    update

--- rk/.hook.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_hook.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /usr/src/linux-headers-6.17.0-19-generic/scripts/module.lds -o hook.ko hook.o hook.mod.o .module-common.o
--- rk/.hook.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_hook.mod := printf '%s\n'   hook.o | awk '!x[$$0]++ { print("./"$$0) }' > hook.mod
